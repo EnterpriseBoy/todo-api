4ae3865984beabc961825cdaa296f3c4bfb0d225
Nial Maguire <niall.mgauire@topmail.ie>
2017-04-04 07:48:26 +0100

    add todos and todos id

--- server.js
+++ server.js
@@ -2,10 +2,32 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false
+},{
+	id: 2,
+	description: 'Go to market',
+	completed: false
+},{
+	id: 3,
+	description: 'Clock in',
+	completed: true
+}]
+
 app.get('/', function(req,res){
 	res.send('To Do API Root');
 });
 
 app.listen(PORT, function(){
-	console.log('Express Running on port' + PORT +'!');
+	console.log('Express Running on port: ' + PORT +'!');
 })
+
+app.get('/todos',function(req,res){
+	res.json(todos);
+});
+
+app.get('/todos/:id',function(req,res{
+	res.send(Asking for todo with id of : + req.params.id);
+}));
