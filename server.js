a5caab43c26ffe19bf560988cd7f98df664d11c2
Nial Maguire <niall.mgauire@topmail.ie>
2017-04-18 07:42:27 +0100

    adding post

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-},{
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},{
-	id: 3,
-	description: 'Clock in',
-	completed: true
-}]
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req,res){
 	res.send('To Do API Root');
 });
-
-app.listen(PORT, function(){
-	console.log('Express Running on port: ' + PORT +'!');
-})
 
 app.get('/todos',function(req,res){
 	res.json(todos);
@@ -32,9 +20,22 @@ app.get('/todos/:id',function(req,res){
 	
 	for (var i in todos) {
 		if(todos[i].id == req.params.id){
-			res.json(todos[req.params.id]);
+			res.json(todos[req.params.id -1]);
 		}
 	}
 	//res.send('Asking for todo with id of :' + req.params.id);
 	res.status(404).send('No information');	
+});
+
+app.post('/todos',function(req,res){
+	var body = req.body;
+	body.id = todoNextId;
+	todoNextId++;
+	todos.push(body);
+	console.log(todos);
+	res.json(body);
+});
+
+app.listen(PORT, function(){
+	console.log('Express Running on port: ' + PORT +'!');
 });
